0e525fc96cfde68964aa678c3f9e96721f8877db
GalaxyCarder <galaxycarder@gmail.com>
2025-01-14 20:05:17 +0530

    Update users.php

--- function/users.php
+++ function/users.php
@@ -1,6 +1,6 @@
 <?php
 
-$owners = ["6895470102", "Radha", "5561431531", "Krishna"];  // Add owner ids here
+$owners = ["6847432039", Galaxy Carder "];  // Add owner ids here
 
 function getUsersCount($filename) {
     if(file_exists($filename)) {
